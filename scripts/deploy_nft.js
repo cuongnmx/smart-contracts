e513ae0f7d725176dfb3e085b20fb0b91f3e8c36
keepondaydreaming <hyouka266@gmail.com>
2021-09-28 00:04:47 +0700

    update

--- scripts/deploy_nft.js
+++ scripts/deploy_nft.js
@@ -2,14 +2,15 @@ const { ethers } = require('hardhat')
 const fs = require('fs')
 
 const deploy = async () => {
-    const HowlToken = await ethers.getContractFactory('HowlToken')
-    const token = await HowlToken.deploy()
-
     let obj = JSON.parse(fs.readFileSync('deployed_address.json'))
-    obj.tokenAddress = token.address
-    fs.writeFileSync('deployed_address.json', JSON.stringify(obj))
 
-    console.log('Contract deploy to a address:', token.address)
+    const GameItem = await ethers.getContractFactory('GameItem')
+    const nft = await GameItem.deploy(obj.marketAddress)
+
+    obj.nftAddress = nft.address
+    fs.writeFileSync('address.json', JSON.stringify(obj))
+
+    console.log('Contract deploy to a address:', nft.address)
 }
 
 deploy()
